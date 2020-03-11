4195c65d74f02fc8e26c398b29e34fc38ed17a24
Abdimalik Mao <AMao@spartaglobal.com>
2020-03-11 13:16:01 +0000

    second commit

--- cookbooks/node_cookbook/recipes/default.rb
+++ cookbooks/node_cookbook/recipes/default.rb
@@ -1,21 +1,18 @@
+#
+# Cookbook:: node_cookbook
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
 include_recipe 'apt'
-include_recipe 'nodejs'
-
-apt_update 'updated_sources' do
+apt_update 'update_sources' do
   action :update
 end
 
 package "nginx"
+
 service "nginx" do
   action [:enable, :start]
 end
-
-
-
-include_recipe "apt"
-include_recipe "nodejs"
-nodejs_npm "pm2"
-
 
 
 template '/etc/nginx/sites-available/proxy.conf' do
@@ -23,7 +20,6 @@ template '/etc/nginx/sites-available/proxy.conf' do
   variables proxy_port: node['nginx']['proxy_port']
   notifies :restart, 'service[nginx]'
 end
-
 
 link '/etc/nginx/sites-enabled/proxy.conf' do
   to '/etc/nginx/sites-available/proxy.conf'
@@ -34,3 +30,6 @@ link '/etc/nginx/sites-enabled/default' do
   action :delete
   notifies :restart, 'service[nginx]'
 end
+
+include_recipe 'nodejs'
+nodejs_npm 'pm2'
--- cookbooks/node_cookbook/test/integration/default/default_test.rb
+++ cookbooks/node_cookbook/test/integration/default/default_test.rb
@@ -1,15 +1,9 @@
-# InSpec test for recipe node_cookbook::default
-
-# The InSpec reference, with examples and extensive documentation, can be
-# found at https://www.inspec.io/docs/reference/resources/
-=begin
 unless os.windows?
   # This is an example test, replace with your own test.
   describe user('root'), :skip do
     it { should exist }
   end
 end
-=end
 
 # This is an example test, replace it with your own test.
 describe port(80) do
@@ -22,10 +16,14 @@ describe service "nginx" do
 end
 
 describe http('http://localhost', enable_remote_worker: true) do
-  its('status') { should cmp 502}
+  its('status') { should cmp 502 }
 end
 
-describe package('nodejs') do
+describe package ('nodejs') do
   it { should be_installed }
-  its('version') { should eq '8.11.2' }
+  its('version') { should cmp > '8.11.2*'}
+end
+
+describe npm ("pm2") do
+  it { should be_installed }
 end
